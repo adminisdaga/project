ef05817efa9fe9b2dfac933a1af2bb80f36f128c
adminisdaga <dmagomedov978@gmail.com>
2025-01-09 22:22:48 +0300

    add fyles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,13 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+ readonly headeritem1 = 'Главная'
+ readonly headeritem2 = 'О компаний'
+ readonly headeritem3 = 'Каталог'
+ readonly header2item = 'Каталог'
+ readonly header2item2 = 'Стройматериалы'
+ readonly header2item3 = 'Инструменты'
+ readonly header2item4 = 'Электрика'
+ readonly header2item5 = 'Интерьер и одежда'
+ 
 }
